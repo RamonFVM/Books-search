a19913aacaa432ebd6d46ee72d10011fb753245d
Ramonfvm <ramonfhelipe547@gmail.com>
2024-12-15 13:06:38 -0300

    Adição de hook

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,4 +1,13 @@
+'use client';
+
+import { useState } from "react";
+
+
 export function Home() {
+
+    const [movie,setmovie]=useState<string>('')
+
+
   return (
     <div className="bg-black min-h-screen flex flex-col">
       <header className="bg-slate-800 font-sans flex justify-center items-center text-white p-6 text-4xl">
@@ -11,11 +20,13 @@ export function Home() {
           <h2 className="text-white text-3xl mb-4">Procure seu livro aqui</h2>
           <input
             type="text"
-            className="p-5 m-4 rounded-2xl text-black w-2/3 focus:outline-none font-semibold"
+            className="p-5 m-4 rounded-2xl w-2/3 focus:outline-none font-semibold"
+
+
             placeholder="Digite o nome do livro"
           />
           
-          <button className="m-6 w-1/4 h-12 rounded-2xl font-semibold bg-white transition-all duration-500 ease-in-out  hover:scale-110 hover:bg-teal-400 hover:text-white" type="submit">
+          <button className="m-6 w-1/4 h-12 rounded-2xl   font-semibold bg-white transition-all duration-500 ease-in-out  hover:scale-110 hover:bg-teal-400 hover:text-white" type="submit">
             Pesquisar
           </button>
 
@@ -24,3 +35,8 @@ export function Home() {
     </div>
   );
 }
+
+
+
+
+
