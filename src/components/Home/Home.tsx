004d7b349dec0ac4288ce1b1faef88f29ef913ad
Ramonfvm <ramonfhelipe547@gmail.com>
2024-12-15 01:29:01 -0300

    First commit

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,12 @@
+export function Home() {
+    return (
+      <div className="bg-black min-h-screen"> 
+        <h1 className="flex justify-center items-center bg-[#5DB996] font-sans text-white p-6 text-4xl">
+          Books Search
+        </h1>
+         <div>
+            
+         </div>
+      </div>
+    );
+  }
